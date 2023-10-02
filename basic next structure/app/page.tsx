d667ebf6ec272a547c384fb8405af5d7872a1e4e
skkingjaat <125859528+skkingjaat@users.noreply.github.com>
2023-10-02 10:41:09 +0530

    Add files via upload

---
+++ basic next structure/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+    <main>
+      <h1 className='text-5xl font-bold'>welcome</h1>
+      
+    </main>
+    </>
+  )
+}
